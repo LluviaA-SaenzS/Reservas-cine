102cd34e9e3ef3d359b33fda32bc09576ca5de34
Rainey S.S <lluviaasaenzs@gmail.com>
2025-10-28 20:34:14 -0600

    creacion de archivos

---
+++ frontend/js/auth.js
@@ -0,0 +1,20 @@
+
+function initAuthButtons() {
+    const loginBtn = document.getElementById('loginBtn');
+    const registerBtn = document.getElementById('registerBtn');
+    
+    if (loginBtn) {
+        loginBtn.addEventListener('click', () => {
+            window.location.href = 'login.html';
+        });
+    }
+    
+    if (registerBtn) {
+        registerBtn.addEventListener('click', () => {
+            window.location.href = 'register.html';
+        });
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', initAuthButtons);
